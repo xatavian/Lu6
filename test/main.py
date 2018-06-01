252ec1671030b65693a283a0ada001adbb27a7bd
Xatyrian <xentsc2@gmail.com>
2018-06-01 21:18:37 +0200

    Cleaned test folder

--- test/main.py
+++
@@ -1,9 +0,0 @@
-import context
-import unittest
-
-from example import ExampleTest
-
-if __name__ == "__main__":
-    suite = unittest.TestSuiet()
-    suite.addTest(ExampleTest())
-    unittest.TextTestRunner().run(suite)
