5028ee1591b090d7a20d29ec4127674ac1fe216b
Avi SZYCHTER <xentsc2@gmail.com>
2018-06-05 09:43:52 +0200

    Added unit tests for print statements

--- lu6/parserengine/__init__.py
+++ lu6/parserengine/__init__.py
@@ -2,4 +2,5 @@ from .compilationunitparser import CompilationUnitParser
 from .statementparser import StatementParser
 from .expressionparser import ExpressionParser
 from .parserhelper import ParserHelper
+from .printparser import PrintParser
 from .parser import Parser
